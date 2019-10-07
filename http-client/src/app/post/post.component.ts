38943b11b61394390cfbfc34d14172489e4e1b2e
Sneha Narkhede <>
2019-10-07 17:14:35 +0530

    Update Search button to display search result in json

--- http-client/src/app/post/post.component.ts
+++ http-client/src/app/post/post.component.ts
@@ -11,37 +11,60 @@ export class PostComponent implements OnInit {
   listOfUserId = [];
   listOfTitle = [];
   actionType;
+  searchResult;
 
   constructor(private pService: PostService) { }
 
   ngOnInit() {
     this.pService.getPostDetail()
-    .subscribe(data => {
-      this.listOfPost = data
-      console.log("list of posts = "+this.listOfPost)
-    })
+      .subscribe(data => {
+        this.listOfPost = data
+        console.log("list of posts = " + this.listOfPost)
+        console.log("server data");
 
-    
+      })
+
+
   }
 
-  loadData(){
-    this.actionType=1
-    
+  loadData() {
+    this.actionType = 1
+    console.log("show data");
+
+
   }
-  
-  showUserId(){
-    this.actionType=2
+
+  showUserId() {
+    this.actionType = 2
+    //   this.pService.getPostDetail()
+    //   .subscribe(data => {
+    //     this.listOfUserId = data
+    //     console.log("list of posts = "+this.listOfUserId)
+    // })
+    console.log("user  data");
+    // console.log(JSON.stringify(this.listOfPost));
+
+  }
+  showTitle() {
+    this.actionType = 3
     // this.pService.getPostDetail()
-    // .subscribe(data => {
-    //   this.listOfUserId = data
-    //   console.log("list of posts = "+this.listOfUserId)
-  // })
-}
-showTitle(){
-  this.actionType=3
-  // this.pService.getPostDetail()
-  // .subscribe(data=>{
-  //   this.listOfTitle=data})
-}
+    // .subscribe(data=>{
+    //   this.listOfTitle=data})
+    console.log("title data");
+
+  }
+  onDataSearch() {
+    console.log("Search is done");
+    this.actionType = 4;
+    // this.searchResult = this.listOfPost[0];
+    for (let i=0; i<this.listOfPost.length; i++){
+      let id = this.listOfPost[i]['id'];
+
+      if(id==5){
+        this.searchResult = this.listOfPost[i];
+        break;
+      }
+    }
+  }
 
 }
